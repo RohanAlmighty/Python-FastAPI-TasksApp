0ccc3f895ac69bbc25be6e681893919f43c5dca1
RohanAlmighty <rohanduttaperfect@gmail.com>
2022-07-15 21:12:14 +0530

    📦 NEW: Added Heroku-Postgres integration

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
 # from env import sqlite_url, postgresql_url
 
 # For SQLLite
@@ -11,9 +13,20 @@ engine = create_engine(
 )
 """
 
-# For PostgreSQL
+# For PostgreSQL-Local
+"""
 SQLALCHEMY_DATABASE_URL = postgresql_url
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+"""
+
+# For Heroku-Postgres
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
+        "postgres://", "postgresql://", 1
+    )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
