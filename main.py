538c904fdcd45f0e27412b8b3ba3462be8df7d77
RohanAlmighty <rohanduttaperfect@gmail.com>
2022-07-23 00:49:26 +0530

    🐛 FIX: Fix root

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
-    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(url="/todos/", status_code=status.HTTP_302_FOUND)
 
 
 app.include_router(auth.router)
